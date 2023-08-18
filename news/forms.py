69eeec4ed581c3077cb84385b8c07373865fbd06
Username-Ewgeniy <114725663+Username-Ewgeniy@users.noreply.github.com>
2023-08-18 19:13:19 +0700

    Delete news directory

--- news/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm, BooleanField 
-from .models import Post
-  
-class PostForm(ModelForm):
-    check_box = BooleanField() 
-    
-    class Meta:
-        model = Post
-        fields = ['title', 'text','postCategory', 'author']
