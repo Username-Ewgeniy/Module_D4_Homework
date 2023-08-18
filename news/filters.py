b660e5258a1e6691d3e0993e88c50f3af6f60267
Username-Ewgeniy <114725663+Username-Ewgeniy@users.noreply.github.com>
2023-08-18 19:14:44 +0700

    Add files via upload

---
+++ news/filters.py
@@ -0,0 +1,17 @@
+from django_filters import FilterSet 
+from .models import Post, Category
+ 
+ 
+
+class PostFilter(FilterSet):
+   
+    class Meta:
+        model = Post
+        fields = {
+            'title': ['icontains'], 
+            
+            'dateCreation': ['gt'], 
+            
+            'text': ['icontains'], 
+            'author': ['exact'],
+        }
